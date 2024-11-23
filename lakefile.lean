1901c966aa1e8b7bbac2e611020164b28dff807c
Patrick Thomas <pthomas505@gmail.com>
2024-11-23 11:12:50 -0800

    Updates.

--- lakefile.lean
+++ lakefile.lean
@@ -11,6 +11,11 @@ package «HPLAR» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+
+require mathlib_extra from git
+  "https://github.com/pthomas505/mathlib_extra.git" @ "main"
+
+
 @[default_target]
 lean_lib «HPLAR» where
   -- add any library configuration options here
